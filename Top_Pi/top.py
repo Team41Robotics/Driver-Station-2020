e7e42e3d620f9d7c29614837ba2ee2bdd031a32d
Peter Linder <palinder@umich.edu>
2020-03-07 11:09:49 -0500

    New climbing interface

--- Top_Pi/top.py
+++ Top_Pi/top.py
@@ -1,7 +1,6 @@
 from button import Button
-from tkinter import Tk, PhotoImage, Canvas, CENTER, RIGHT, LEFT, N
+from tkinter import Tk, Canvas, CENTER, RIGHT, LEFT, N
 import math
-from PIL import Image, ImageTk
 import os
 
 dirname = os.path.dirname(__file__)
@@ -29,43 +28,40 @@ if not test:
 height = 480
 width = 800
 color_inactive = "white"
-color_active = "green"
+color_active = "#32CD32" # lime green
 
 # Delay between pushes (ms)
 delay = 10
 
 # Limelight zoom buttons
-btnW = 100
-btnH = 100
-btnX = 425
-btnY = 115
-padding = 25
-b1 = Button('1x', btnX, btnY, btnW, btnH, '1x', color_active, 32)
-b2 = Button('2x', btnX + btnW + padding, btnY, btnW, btnH, '2x', color_inactive, 32)
-b3 = Button('3x', btnX + (btnW + padding)*2, btnY, btnW, btnH, '3x', color_inactive, 32)
-
-# Intake mode buttons
 btnW = 170
 btnH = 120
 btnX = 420
+btnY = 115
+padding = 20
+b1 = Button('1x', btnX, btnY, btnW, btnH, '1x', color_active, 32)
+b2 = Button('2x', btnX + btnW + padding, btnY, btnW, btnH, '2x', color_inactive, 32)
+
+# Intake mode buttons
 btnY = 340
 padding = 20
-b4 = Button('manual', btnX, btnY, btnW, btnH, 'Manual', color_active, 32)
-b5 = Button('auto', btnX + btnW + padding, btnY, btnW, btnH, 'Auto', color_inactive, 32)
+b3 = Button('manual', btnX, btnY, btnW, btnH, 'Manual', color_active, 32)
+b4 = Button('auto', btnX + btnW + padding, btnY, btnW, btnH, 'Auto', color_inactive, 32)
 
 # Hook buttons
-hook_imgs = ['hook0.gif','hook1.gif','hook2.gif']
-hook_msgs = ['Go to pos 1', 'Go to pos 2', 'In manual']
-hook_colors = ['white', 'yellow', 'orange']
-hook_imgs = [Image.open(os.path.join(dirname, x)) for x in hook_imgs]
-hook_imgs = [ImageTk.PhotoImage(x) for x in hook_imgs]
-b6 = Button('go', 100, 350, 200, 100, hook_msgs[0], hook_colors[0], 24)
+hook_msgs = ['None', 'Start Up', 'Clamp Down', 'Piston Forward', 'Piston Reverse', 'Fork Up', 'Final Up', 'Done']
+num_msgs = len(hook_msgs)
+btnX = 20
+btnW = 2*btnW + padding
+btnH = 100
+btnY = 360
+b5 = Button('next', btnX, btnY, btnW, btnH, 'NEXT', color_inactive, 24)
 
 # Exit button - top left
 ex = Button('exit', 0, 0, 25, 25, "X", "red")
 
 # All buttons
-btns = [b1, b2, b3, b4, b5, b6, ex]
+btns = [b1, b2, b3, b4, b5, ex]
 
 # Canvas object
 ctx = Canvas(root, width=width, height=height, background="black")
@@ -77,6 +73,7 @@ ctx.create_line(width/2, height/2, width, height/2, fill="white")
 # Text at top
 ctx.create_text(600, 60, justify=CENTER, text="Limelight zoom", font=("Arial", 28), fill="White")
 ctx.create_text(600, 300, justify=CENTER, text="Intake mode", font=("Arial", 28), fill="White")
+ctx.create_text(200, 60, justify=CENTER, text="Current state", font=("Arial", 28), fill="White")
 
 # State variables
 zoom = 1
@@ -96,42 +93,39 @@ def handle_click(event):
     if b1.isClicked:
         b1.color = color_active
         b2.color = color_inactive
-        b3.color = color_inactive
         zoom = 1
     elif b2.isClicked:
         b1.color = color_inactive
         b2.color = color_active
-        b3.color = color_inactive
         zoom = 2
-    elif b3.isClicked:
-        b1.color = color_inactive
-        b2.color = color_inactive
+
+    if b3.isClicked:
         b3.color = color_active
-        zoom = 3
-
-    if b4.isClicked:
-        b4.color = color_active
-        b5.color = color_inactive
-        mode = 0
-    elif b5.isClicked:
         b4.color = color_inactive
-        b5.color = color_active
+        mode = 0
+    elif b4.isClicked:
+        b3.color = color_inactive
+        b4.color = color_active
         mode = 1
-
-    if b6.isClicked:
+	
+    if b5.isClicked:
         hook += 1
-        if hook > 2: hook = 0
-        b6.text_active = hook_msgs[hook]
-        b6.color = hook_colors[hook]
+        b5.text_active = "RESET" if hook == num_msgs-1 else "NEXT"
+        if hook >= num_msgs: hook = 0
     
     drawStuff()
 
 
 def drawStuff():
     for btn in btns: btn.drawButton(ctx)
-
-    ctx.delete('hook_img')
-    ctx.create_image(200, 10, anchor=N, image=hook_imgs[hook], tag='hook_img')
+    
+    ctx.delete('curr_msg')
+    ctx.delete('next_msg')
+    ctx.delete('next_title')
+    ctx.create_text(200, 120, justify=CENTER, text=hook_msgs[hook], font=("Arial", 36), fill=color_active, tag="curr_msg")
+    if hook < num_msgs-1:
+        ctx.create_text(200, 240, justify=CENTER, text="Next state", font=("Arial", 28), fill="White", tag="next_title")
+        ctx.create_text(200, 300, justify=CENTER, text=hook_msgs[hook+1], font=("Arial", 36), fill=color_active, tag="next_msg")
 
 
 drawStuff()
@@ -141,11 +135,10 @@ ctx.pack()
 
 
 def publish():
-    val = 0
-    if hook > 0:
-        val = ((hook - 1) << 2) | 3 # 3 if hook == 1, 7 if hook == 2
-    else:
-        val = (mode << 2) | (zoom - 1) # Lower 2 bits are zoom, third bit is mode
+    hook_bit = hook & 1
+    val = (hook_bit << 2) | (mode << 1) | (zoom - 1) # Lower bit is zoom, second bit is mode, third bit is hook toggle
+    if test:
+        print('{0:04b}'.format(val))
     ser.write(val.to_bytes(1, byteorder='big'))
     root.after(delay, publish)
 
