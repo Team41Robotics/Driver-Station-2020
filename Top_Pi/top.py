92326505c915a74ba27a7b87b86a44c92ca348d9
Mason <mason@digiorgiofamily.net>
2022-03-03 08:56:51 -0500

    Updated limelight zoom panel for 2022

--- Top_Pi/top.py
+++ Top_Pi/top.py
@@ -1,5 +1,5 @@
 from button import Button
-from tkinter import Tk, Canvas, CENTER, RIGHT, LEFT, N
+from tkinter import Tk, Canvas, CENTER, RIGHT, LEFT, N, PhotoImage
 import math
 import os
 
@@ -34,46 +34,45 @@ color_active = "#32CD32" # lime green
 delay = 10
 
 # Limelight zoom buttons
-btnW = 170
-btnH = 120
-btnX = 420
-btnY = 115
-padding = 20
-b1 = Button('1x', btnX, btnY, btnW, btnH, '1x', color_active, 32)
-b2 = Button('2x', btnX + btnW + padding, btnY, btnW, btnH, '2x', color_inactive, 32)
-
 # Intake mode buttons
 btnY = 340
 padding = 20
-b3 = Button('manual', btnX, btnY, btnW, btnH, 'Manual', color_active, 32)
-b4 = Button('auto', btnX + btnW + padding, btnY, btnW, btnH, 'Auto', color_inactive, 32)
 
 # Hook buttons
-hook_msgs = ['None', 'Start Up', 'Clamp Down', 'Piston Forward', 'Piston Reverse', 'Fork Up', 'Final Up', 'Done']
-num_msgs = len(hook_msgs)
 btnX = 20
-btnW = 2*btnW + padding
+btnW = 2*250 + padding
 btnH = 100
 btnY = 360
-b5 = Button('next', btnX, btnY, btnW, btnH, 'NEXT', color_inactive, 24)
 
 # Exit button - top left
 ex = Button('exit', 0, 0, 25, 25, "X", "red")
 
-# All buttons
-btns = [b1, b2, b3, b4, b5, ex]
 
+btnW = 250
+btnH = 120
+btnX = 420
+btnY = 115
+padding = 20
+b1 = Button('1x', width/3 - width/4, height/3, btnW, btnH, '1x', color_active, 32)
+b2 = Button('2x', width*2/3 - btnW +  width/4, height/3, btnW, btnH, '2x', color_inactive, 32)
+
+# All buttons
+btns = [b1, b2, ex]
+
+zoom1x = PhotoImage(file="1xzoom.gif")
+zoom2x = PhotoImage(file="2xzoom.gif")
 # Canvas object
 ctx = Canvas(root, width=width, height=height, background="black")
 ctx.pack()
 
-ctx.create_line(width/2, 0, width/2, height, fill="white")
-ctx.create_line(width/2, height/2, width, height/2, fill="white")
 
 # Text at top
-ctx.create_text(600, 60, justify=CENTER, text="Limelight zoom", font=("Arial", 28), fill="White")
-ctx.create_text(600, 300, justify=CENTER, text="Intake mode", font=("Arial", 28), fill="White")
-ctx.create_text(200, 60, justify=CENTER, text="Current state", font=("Arial", 28), fill="White")
+ctx.create_text(width/2, height/5, justify=CENTER, text="Limelight zoom", font=("Arial", 28), fill="White")
+
+ctx.create_image(width/3-width/4+b1.wdith/2, height*4/5, image=zoom1x, tag="1xzoom", anchor=CENTER)
+# ctx.create_rectangle(width/3-width/4, height*4/5-90, width/3-width/4+b1.wdith, height*4/5+b1.height/2, outline="green", tag="1xzoomrect")
+ctx.create_image(width*2/3 - btnW/2 +  width/4, height*4/5, image=zoom2x, tag="2xzoom", anchor=CENTER)
+# ctx.create_rectangle(width/3-width/4, height*4/5-90, width/3-width/4+b1.wdith, height*4/5+b1.height/2, outline="green", tag="1xzoomrect")
 
 # State variables
 zoom = 1
@@ -81,7 +80,7 @@ mode = 0
 hook = 0
 
 def handle_click(event):
-    global zoom, mode, hook
+    global zoom, mode
     if not test:
         event.x = math.fabs(event.x-800)
         event.y = math.fabs(event.y-480)
@@ -98,20 +97,6 @@ def handle_click(event):
         b1.color = color_inactive
         b2.color = color_active
         zoom = 2
-
-    if b3.isClicked:
-        b3.color = color_active
-        b4.color = color_inactive
-        mode = 0
-    elif b4.isClicked:
-        b3.color = color_inactive
-        b4.color = color_active
-        mode = 1
-	
-    if b5.isClicked:
-        hook += 1
-        b5.text_active = "RESET" if hook == num_msgs-1 else "NEXT"
-        if hook >= num_msgs: hook = 0
     
     drawStuff()
 
@@ -122,10 +107,6 @@ def drawStuff():
     ctx.delete('curr_msg')
     ctx.delete('next_msg')
     ctx.delete('next_title')
-    ctx.create_text(200, 120, justify=CENTER, text=hook_msgs[hook], font=("Arial", 36), fill=color_active, tag="curr_msg")
-    if hook < num_msgs-1:
-        ctx.create_text(200, 240, justify=CENTER, text="Next state", font=("Arial", 28), fill="White", tag="next_title")
-        ctx.create_text(200, 300, justify=CENTER, text=hook_msgs[hook+1], font=("Arial", 36), fill=color_active, tag="next_msg")
 
 
 drawStuff()
